ac3add09fd6e6f8cb76632f43fddc248b1c3d964
Angie  Flower <82205086+AngieFlower@users.noreply.github.com>
2021-05-17 15:26:13 +0200

    paso2

---
+++ src/main/java/org/factoriaf5/store/controller/ProductController.java
@@ -0,0 +1,17 @@
+package org.factoriaf5.store.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController{
+
+    @GetMapping("/health")
+
+    public String check(){
+
+        return "Hello I'm a server";
+
+    }
+}
---
+++ src/main/java/org/factoriaf5/store/controller/metodocheck.java
@@ -0,0 +1,4 @@
+package org.factoriaf5.store.controller;
+
+public class metodocheck {
+}
